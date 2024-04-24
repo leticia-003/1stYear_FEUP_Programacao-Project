a3b30254a2886595291dfd24d2df5dc2aafd30f9
Letícia Coelho <leticiamendes2122@gmail.com>
2024-04-24 19:21:52 +0100

    first group working

--- readSVG.cpp
+++ readSVG.cpp
@@ -29,6 +29,8 @@ namespace svg
     }
     template<typename T>
 
+
+
     void parseTransform(T& element, const string& transform, const Point& transformOrigin) {
         stringstream ss(transform);
         string operation;
@@ -64,6 +66,131 @@ namespace svg
                 int factor;
                 sscanf(operation.c_str(), "scale(%d)", &factor);
                 element.addTransformation([&element, factor, &transformOrigin]() { element.scale(transformOrigin, factor); });
+            }
+        }
+    }
+
+    void parseSVGElement(XMLElement* element, const Point& transformOrigin, vector<SVGElement *>& svg_elements) {
+        const string nodeName = element->Name();
+        const char* attrValue = element->Attribute("transform");
+        string transform = attrValue ? attrValue : "";  // Use empty string if null
+
+        Point newTransformOrigin = transformOrigin; // By default, use parent's transform origin
+
+        const char* originAttr = element->Attribute("transform-origin");
+        string transformOriginStr = originAttr ? originAttr : "0 0";  // Provide a default "0 0" if not present
+        sscanf(transformOriginStr.c_str(), "%d %d", &newTransformOrigin.x, &newTransformOrigin.y);
+
+        if (nodeName == "g") {
+            XMLElement* child = element->FirstChildElement();
+            while (child != nullptr) {
+                parseSVGElement(child, newTransformOrigin, svg_elements);
+                child = child->NextSiblingElement();
+            }
+        } else {
+            // Determine SVG element type
+            if (nodeName == "ellipse") {
+
+                int cx = element->IntAttribute("cx");
+                int cy = element->IntAttribute("cy");
+                int rx = element->IntAttribute("rx");
+                int ry = element->IntAttribute("ry");
+                string fill = element->Attribute("fill");
+                Color fillColor = parse_color(fill);
+
+                // Create center and radius points
+                Point center;
+                center.x = cx;
+                center.y = cy;
+
+                Point radius;
+                radius.x = rx;
+                radius.y = ry;
+
+                // Create Ellipse and store it
+                Ellipse* ellipse = new Ellipse(fillColor, center, radius);
+                ellipse->setTransformOrigin(transformOrigin);
+                parseTransform(*ellipse, transform, transformOrigin);
+                ellipse->applyTransformations();
+                svg_elements.push_back(ellipse);
+
+            } else if (nodeName == "circle") {
+
+                int cx = element->IntAttribute("cx");
+                int cy = element->IntAttribute("cy");
+                int r = element->IntAttribute("r");
+                string fill = element->Attribute("fill");
+                Color fillColor = parse_color(fill);
+
+                Circle* circle = new Circle(fillColor, Point{cx, cy}, r);
+                circle->setTransformOrigin(transformOrigin);
+                parseTransform(*circle, transform, transformOrigin);
+                circle->applyTransformations();
+                svg_elements.push_back(circle);
+
+            } else if (nodeName == "polyline") {
+
+                string points_str = element->Attribute("points");
+                vector<Point> points = parse_points(points_str);
+                string stroke = element->Attribute("stroke");
+                Color strokeColor = parse_color(stroke);
+                Polyline* polyline = new Polyline(strokeColor, points);
+
+                polyline->setTransformOrigin(transformOrigin);
+                parseTransform(*polyline, transform, transformOrigin);
+                polyline->applyTransformations();
+
+                svg_elements.push_back(polyline);
+
+            } else if (nodeName == "line") {
+
+                int x1 = element->IntAttribute("x1");
+                int y1 = element->IntAttribute("y1");
+                int x2 = element->IntAttribute("x2");
+                int y2 = element->IntAttribute("y2");
+                string stroke = element->Attribute("stroke") ? element->Attribute("stroke") : "black";
+
+                Color strokeColor = parse_color(stroke);
+                Line* line = new Line(strokeColor, Point{x1, y1}, Point{x2, y2});
+
+                line->setTransformOrigin(transformOrigin);
+                parseTransform(*line, transform, transformOrigin);
+                line->applyTransformations();
+
+                svg_elements.push_back(line);
+
+            } else if (nodeName == "polygon") {
+
+                string points_str = element->Attribute("points");
+                vector<Point> points = parse_points(points_str);
+                string fill = element->Attribute("fill");
+                Color fillColor = parse_color(fill);
+
+                Polygon* polygon = new Polygon(fillColor, points);
+
+                polygon->setTransformOrigin(transformOrigin);
+                parseTransform(*polygon, transform, transformOrigin);
+                polygon->applyTransformations();
+
+                svg_elements.push_back(polygon);
+
+            } else if (nodeName == "rect") {
+
+                int x = element->IntAttribute("x");
+                int y = element->IntAttribute("y");
+                int width = element->IntAttribute("width");
+                int height = element->IntAttribute("height");
+                string fill = element->Attribute("fill");
+                Color fillColor = parse_color(fill);
+                width -= 1;
+                height -= 1;
+
+                Rectangle* rectangle = new Rectangle(fillColor, Point{x, y}, width, height);
+                rectangle->setTransformOrigin(transformOrigin);
+                parseTransform(*rectangle, transform, transformOrigin);
+                rectangle->applyTransformations();
+                svg_elements.push_back(rectangle);
+
             }
         }
     }
@@ -218,10 +345,17 @@ namespace svg
                 svg_elements.push_back(rectangle);
             }
 
+            else if (nodeName == "g")
+            {
+                // If this is a <g> element, parse its children
+                parseSVGElement(child, transformOrigin, svg_elements);
+            }
+
 
             // Move to the next child
             child = child->NextSiblingElement();
         }
         
     }
+
 }
